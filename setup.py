991d291c2c7b1ff4d34d3dd3af080b6fdaf4b046
Kilian Klimek <kilian.klimek@googlemail.com>
2013-05-04 23:35:38 +0200

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 setup(
     name = 'ubervisor',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'ubervisor client',
     author = 'Kilian Klimek',
     author_email = 'kilian.klimek@googlemail.com',
